2558f198b9702f8cce9aff52d16e21cf5b62b687
1goresha <i.deryabin2017@mail.ru>
2020-09-25 17:20:42 +0300

    add java methods to buttons

--- MusicShop/app/src/main/java/com/example/musicshop/MainActivity.java
+++ MusicShop/app/src/main/java/com/example/musicshop/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.musicshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+    TextView quantityTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quantityTextView = findViewById(R.id.quantity);
+    }
+
+    public void increase(View view) {
+        quantityTextView.setText(++quantity + "");
+    }
+
+    public void decrease(View view) {
+        quantity--;
+        if (quantity < 0){
+            quantity++;
+        }
+        quantityTextView.setText(quantity + "");
     }
 }
