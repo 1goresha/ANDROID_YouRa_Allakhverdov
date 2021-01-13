f10cc02c3196eb5a7aaa1066fc2a5f404992bb03
1goresha <i.deryabin2017@mail.ru>
2021-01-14 02:28:42 +0300

    add some code

--- CoolTimer/app/src/main/java/com/example/cooltimer/SettingsFragment.java
+++ CoolTimer/app/src/main/java/com/example/cooltimer/SettingsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 
 public class SettingsFragment extends PreferenceFragmentCompat {//предназначен для отображения фрагмента с настройками из файла timer_preferences.xml
@@ -21,15 +22,22 @@ public class SettingsFragment extends PreferenceFragmentCompat {//предназ
         String value;
         for (int i = 0; i < count; i++) {
             Preference preference = preferenceScreen.getPreference(i);
+            int a;
             if (preference instanceof ListPreference) {
                 value = sharedPreferences.getString(preference.getKey(), "");// в SharedPreference находятся все ключ-значения
                 // , которые туда сохраняются и от туда же их можно извлечь, используя метод getString(preference.getKey(),"")
                 ListPreference listPreference = (ListPreference) preference;
                 listPreference.setSummary(value);
             }
-            int a = 0;
-
         }
+
+        preferenceScreen.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                return false;
+            }
+        });
     }
+
 
 }
